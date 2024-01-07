4d55ec0f51fbf7d0058773d4c89b4c1fc6c08d9e
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2024-01-07 19:21:15 +0100

    Added test app

---
+++ app/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Net.Sockets;
+
+try
+{
+    throw new SocketException(10014);
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Example exception: {ex}");
+}
+
+Console.WriteLine();
+Console.WriteLine("Press any key to close");
+Console.ReadKey();
