729fdecdcea030b0ceac90300217c237a0d194a7
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2024-01-07 02:00:22 +0100

    Skeletons

---
+++ src/Initializer.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Nefarius.Utilities.EnglishExceptions;
+
+internal sealed class Initializer
+{
+    [ModuleInitializer]
+    public static void Initialize()
+    {
+        // TODO: implement me
+    }
+}
---
+++ src/Util/LangUtils.cs
@@ -0,0 +1,28 @@
+﻿using System.Globalization;
+using System.Runtime.InteropServices;
+
+namespace Nefarius.Utilities.EnglishExceptions.Util;
+
+internal static class LangUtils
+{
+    public static IEnumerable<CultureInfo> InstalledInputLanguages
+    {
+        get
+        {
+            // first determine the number of installed languages
+            uint size = GetKeyboardLayoutList(0, null);
+            IntPtr[] ids = new IntPtr[size];
+
+            // then get the handles list of those languages
+            GetKeyboardLayoutList(ids.Length, ids);
+
+            foreach (int id in ids) // note the explicit cast IntPtr -> int
+            {
+                yield return new CultureInfo(id & 0xFFFF);
+            }
+        }
+    }
+
+    [DllImport("user32.dll")]
+    private static extern uint GetKeyboardLayoutList(int nBuff, [Out] IntPtr[] lpList);
+}
